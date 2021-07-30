56d4dddbe184ab1b709a46fcc3a11804e69d340c
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-07-30 09:28:26 +0300

    working on hamburger

--- app/components/hamburger-menu/hamburger-menu.js
+++ app/components/hamburger-menu/hamburger-menu.js
@@ -1,33 +1,11 @@
-alert(123)
-
 const leftMenuSwiper = new Swiper('.hamburger__left-menu', {
-	/*   spaceBetween: 10, */
 	   direction: 'vertical',
-	 /*  centeredSlides: true, */
+	   slidesPerView: 1,
 	  slidesPerView: 'auto',
-	  /* loopedSlides: 3, */
-	  /* touchRatio: 0.3, */
-	 /*  slideToClickedSlide: true, */
 	 loop: false,
 	  
 	  watchSlidesVisibility: true,
 	  watchSlidesProgress: true,
-	  breakpoints: {
-	    320: {
-	      direction: 'horizontal',
-	      slidesPerView: 3,
-	      spaceBetween: 10,
-	    },
-	  768: {
-	    direction: 'vertical',
-	    spaceBetween: 14,
-	    
-	  },
-	  1280: {
-	    direction: 'vertical',
-	    spaceBetween: 10,
-	  },
-	},
 	  
 	});
 	
@@ -42,16 +20,7 @@ const leftMenuSwiper = new Swiper('.hamburger__left-menu', {
 	  },
 	
 
-	  breakpoints: {
-	    320: {
-	      autoHeight: true,
-	      spaceBetween: 5,
-	    },
-	  768: {
-	    autoHeight: false,
-	    spaceBetween: 10,
-	  },
-	},
+
 	});
 
 
