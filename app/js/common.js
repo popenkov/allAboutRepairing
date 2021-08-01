f38579288b6abe1a1dd4ad444c2be6871f0eebc2
TonyRandom <65846634+TonyRandom@users.noreply.github.com>
2021-08-01 13:55:11 +0300

    desktop version

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //= ../components/hamburger-menu/hamburger-menu.js
     //= ../components/main-page/main-slider/main-slider.js
     //= ../components/main-page/pop-works/pop-works.js
+    //= ../components/main-page/new-videos/new-videos.js
     //= ../components/header/header.js
     
 
