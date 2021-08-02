d3df2ac6827319ee34ada5038bf2ebbd1df15020
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-08-02 16:25:13 +0300

    switched to new videos

---
+++ app/components/main-page/pop-articles/pop-articles.js
@@ -0,0 +1,62 @@
+
+
+let { clientWidth } = document.body;
+const swiperContainer = document.querySelector('.pop-articles__items-swiper');
+let popArticlesSwiper;
+const sliderInit = () => {
+    console.log('sliderInit')
+   popArticlesSwiper  = new Swiper('.pop-articles__items-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+
+  
+  });
+
+  swiperContainer.classList.remove('pop-articles--no-swiper');
+}
+
+const swiperDestroy = () => {
+    console.log('swiperDestroy')
+    popArticlesSwiper.destroy(true, true)
+    swiperContainer.classList.add('pop-articles--no-swiper');
+}
+
+
+if (clientWidth< 415) {
+    sliderInit();
+}
+
+const resizeHandlerSlider = () => {
+    let { clientWidth } = document.body;
+    console.log(clientWidth);
+    if (clientWidth< 415) {
+        sliderInit();
+    } else if (clientWidth >= 415 && swiperContainer.classList.contains('swiper-container-initialized')) {
+
+        swiperDestroy(); 
+    }
+}
+
+window.addEventListener('resize', resizeHandlerSlider);
+
+
+
+
+console.log(clientWidth)
+/* let yourSlider;
+
+
+
+const resizeHandlerSlider = () => {
+    if (clientWidth !== document.body.clientWidth) {
+        clientWidth = document.body.clientWidth;
+
+        if (yourSlider) {
+            yourSlider.destroy();
+        }
+
+        sliderInit();
+    }
+} */
--- app/js/common.js
+++ app/js/common.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //= ../components/main-page/main-slider/main-slider.js
     //= ../components/main-page/pop-works/pop-works.js
     //= ../components/main-page/new-videos/new-videos.js
+    //= ../components/main-page/pop-articles/pop-articles.js
     //= ../components/header/header.js
     
 
