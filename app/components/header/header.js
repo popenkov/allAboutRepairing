65d244db0352c12ef51944d32525f2a894d25a45
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-08-02 17:07:26 +0300

    new videos, subscription and footer on mobile made

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -5,3 +5,13 @@ const headerLocationPopup = document.querySelector('.header__location-popup');
     e.preventDefault();
     headerLocationPopup.classList.toggle('header__location-popup--show');
 })
+
+
+
+
+const headerButtonMobile = document.querySelector('.header__hamburger-btn-mobile');
+
+!!headerButtonMobile && headerButtonMobile.addEventListener('click', (e) => {
+    e.preventDefault();
+    headerLocationPopup.classList.toggle('header__location-popup--show');
+})
