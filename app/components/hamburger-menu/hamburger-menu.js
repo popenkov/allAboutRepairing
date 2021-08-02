7e9753b99d55d6dcf7c7fa3b51ea942f75c732a0
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-08-02 09:01:57 +0300

    finishingpop articles

--- app/components/hamburger-menu/hamburger-menu.js
+++ app/components/hamburger-menu/hamburger-menu.js
@@ -1,35 +1,49 @@
-const leftMenuSwiper = new Swiper('.hamburger__left-menu', {
-	   direction: 'vertical',
-	   slidesPerView: 1,
-	  slidesPerView: 'auto',
-	  allowTouchMove: false,
-	 loop: false,
-	  
-	  watchSlidesVisibility: true,
-	  watchSlidesProgress: true,
 
-	  scrollbar: {
-		el: '.swiper-scrollbar',
-		draggable: true,
-		hide: true,
-	  },
-	  
-	});
+
+
+	const catalogButton = document.querySelector('.js-hamburger-btn');
+	const hamburgerMenu = document.querySelector('.js-hamburger-menu');
 	
-
-	const rightMenuSwiper = new Swiper('.hamburger__right-menu', {
-	  
-	  slidesPerView: 1,
-	  spaceBetween: 50,
-	  direction: 'vertical',
-	  allowTouchMove: false,
-		loop: false,
-		thumbs: {
-		swiper: leftMenuSwiper,
-		},
+	!!catalogButton & catalogButton.addEventListener('click', () => {
+		hamburgerMenu.classList.toggle('hamburger-show');
 
 
-
-	});
-
-
+		const leftMenuSwiper = new Swiper('.hamburger__left-menu', {
+			direction: 'vertical',
+			slidesPerView: 1,
+		   slidesPerView: 'auto',
+		   allowTouchMove: false,
+		  loop: false,
+		   
+		   watchSlidesVisibility: true,
+		   watchSlidesProgress: true,
+	 
+		   scrollbar: {
+			 el: '.swiper-scrollbar',
+			 draggable: true,
+			 hide: true,
+		   },
+		   
+		 });
+		 
+	 
+		 const rightMenuSwiper = new Swiper('.hamburger__right-menu', {
+		   
+		   slidesPerView: 1,
+		   spaceBetween: 50,
+		   direction: 'vertical',
+		   allowTouchMove: false,
+			 loop: false,
+			 thumbs: {
+			 swiper: leftMenuSwiper,
+			 },
+	 
+	 
+	 
+		 });
+	})
+	
+	
+	
+	
+	
