793f1c8319719a443476a9531fe311aafaaf8234
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-08-03 10:15:37 +0300

    header hamburger js made

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -10,8 +10,37 @@ const headerLocationPopup = document.querySelector('.header__location-popup');
 
 
 const headerButtonMobile = document.querySelector('.header__hamburger-btn-mobile');
+const menuContainer = document.querySelector('.header__navigation-container');
+const main = document.querySelector('main');
+const siteBody = document.querySelector('body');
+ 
+
+
+
+const toogleHamburger=() => {
+    headerButtonMobile.classList.toggle("change");
+};
+
+const showMenu = () => {
+    menuContainer.classList.toggle('show');
+    /* siteBody.classList.toggle('body__no-scroll'); */
+
+}
+
 
 !!headerButtonMobile && headerButtonMobile.addEventListener('click', (e) => {
     e.preventDefault();
-    headerLocationPopup.classList.toggle('header__location-popup--show');
+    toogleHamburger();
+    showMenu();
+
+
+  /*  if(menuContainer.classList.contains('show')) {
+        main.style.display =  'none';
+    } else {
+        main.style.display =  'block';
+    } */
+
 })
+
+
+
