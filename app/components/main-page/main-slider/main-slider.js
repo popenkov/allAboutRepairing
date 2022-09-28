5f20cd77dc62d7427d9659a84072a425405018f6
Popenkov Anton <popenkovaa@yandex.ru>
2022-09-28 07:50:35 +0300

    start translate and fixing bugs

--- app/components/main-page/main-slider/main-slider.js
+++ app/components/main-page/main-slider/main-slider.js
@@ -1,13 +1,12 @@
 const mainSliderswiper = new Swiper('.main__slider-swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 20,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.main__slider-button-next',
-      prevEl: '.main__slider-button-prev',
-    },
-  
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.main__slider-button-next',
+    prevEl: '.main__slider-button-prev',
+  },
+});
